2b3a5ddd5118c7be0aa8ea852c0a7f3ff920e13e
Pasztor Isti <isticsek@gmail.com>
2025-12-31 16:47:51 +0200

    create 2 custom pages

---
+++ app/(tabs)/clients.tsx
@@ -0,0 +1,14 @@
+import ThemeToggler from '@/components/theme-toggler';
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+const Clients = () => {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text variant="h3">Clients</Text>
+      <ThemeToggler />
+    </View>
+  );
+};
+
+export default Clients;
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+const Profile = () => {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text variant="h3">Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
