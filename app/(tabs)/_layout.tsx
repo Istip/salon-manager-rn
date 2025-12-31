766e5c4765eab56fe1bd29e8396554dd14010f78
Pasztor Isti <isticsek@gmail.com>
2025-12-31 19:25:23 +0200

    configure tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,70 @@
+import { Icon } from '@/components/ui/icon';
+import { colors } from '@/lib/colors';
 import { Tabs } from 'expo-router';
-import React from 'react';
+import { Clock, Settings, UsersRound } from 'lucide-react-native';
 
 const TabsLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="clients" options={{ title: 'Clients', headerShown: false }} />
-      <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile', headerShown: false }} />
+    <Tabs
+      initialRouteName="index"
+      screenOptions={{
+        animation: 'shift',
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 80,
+          borderTopWidth: 0,
+        },
+        tabBarIconStyle: {
+          marginVertical: 'auto',
+        },
+        tabBarActiveBackgroundColor: colors.primary,
+      }}>
+      <Tabs.Screen
+        name="clients"
+        options={{
+          title: 'Clients',
+          headerShown: false,
+          animation: 'shift',
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              as={UsersRound}
+              size={25}
+              className={focused ? 'text-foreground' : 'text-muted-foreground'}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false,
+          animation: 'shift',
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              as={Clock}
+              size={25}
+              className={focused ? 'text-foreground' : 'text-muted-foreground'}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          headerShown: false,
+          animation: 'shift',
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              as={Settings}
+              size={25}
+              className={focused ? 'text-foreground' : 'text-muted-foreground'}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
-
 export default TabsLayout;
