f2031844ebbfb697cbead04fe60c4f9f813f7002
Pasztor Isti <isticsek@gmail.com>
2025-12-31 15:33:09 +0200

    update not found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,15 +1,15 @@
+import { Text } from '@/components/ui/text';
 import { Link, Stack } from 'expo-router';
 import { View } from 'react-native';
-import { Text } from '@/components/ui/text';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View>
-        <Text>This screen doesn't exist.</Text>
+        <Text>This screen doesn&apos;t exist.</Text>
 
-        <Link href="/">
+        <Link href="/index">
           <Text>Go to home screen!</Text>
         </Link>
       </View>
