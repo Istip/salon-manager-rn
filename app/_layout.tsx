8b05df2b88e40d8c883f5c01487efcb30f9d951b
Pasztor Isti <isticsek@gmail.com>
2025-12-31 16:43:25 +0200

    modify stack screens to hide headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,12 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ title: 'Home', headerShown: false, animation: 'fade' }}
+        />
+      </Stack>
       <PortalHost />
     </ThemeProvider>
   );
