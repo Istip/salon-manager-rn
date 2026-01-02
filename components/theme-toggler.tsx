819f1ca6eb9da6d380bac57558d1992b2d2ce700
Pasztor Isti <isticsek@gmail.com>
2026-01-02 22:03:35 +0200

    add classname prop for theme toggler for further update

--- components/theme-toggler.tsx
+++ components/theme-toggler.tsx
@@ -1,11 +1,14 @@
 import Switcher from '@/components/switcher';
 import { useColorScheme } from 'nativewind';
 
-const ThemeToggler = () => {
+const ThemeToggler = ({ className }: { className?: string }) => {
   const { colorScheme, toggleColorScheme } = useColorScheme();
 
   return (
-    <Switcher value={colorScheme === 'dark'} onValueChange={toggleColorScheme}>
+    <Switcher
+      value={colorScheme === 'dark'}
+      onValueChange={toggleColorScheme}
+      className={className}>
       Use dark mode
     </Switcher>
   );
