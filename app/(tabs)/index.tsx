833b08d7b977205d3d5dd6603b7021f45b4cb03d
Pasztor Isti <isticsek@gmail.com>
2026-01-03 19:05:59 +0200

    update calendar page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,12 @@
+import CalendarEvents from '@/components/calendar/calendar-events';
 import CalendarNavigator from '@/components/calendar/calendar-navigator';
-import { Text } from '@/components/ui/text';
-import { formatDate } from '@/lib/date-functions';
-import { useDateStore } from '@/lib/stores/date-store';
-import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Calendar() {
-  const selectedDay = useDateStore((state) => state.selectedDay);
-
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1">
       <CalendarNavigator />
-      <ScrollView>
-        <View className="flex-1 items-center justify-center gap-8 px-4">
-          <Text variant="h2">Calendar Tab</Text>
-          <Text variant="muted">{formatDate(selectedDay)}</Text>
-        </View>
-      </ScrollView>
+      <CalendarEvents />
     </SafeAreaView>
   );
 }
