3180a3f2a7f953babab006a4583b381a44b0f659
Pasztor Isti <isticsek@gmail.com>
2026-01-03 15:58:54 +0200

    boilerplate for top calendar navigator

---
+++ components/calendar/calendar-navigator.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+const CalendarNavigator = () => {
+  return (
+    <View className="mb-4 w-full border-b border-b-stone-300 bg-background p-4 dark:border-b-black">
+      <Text>This will render the calendar navigator</Text>
+    </View>
+  );
+};
+
+export default CalendarNavigator;
