f18eccb7ef8aabdf3dcb46ba2b28e1a83d1435b0
Pasztor Isti <isticsek@gmail.com>
2026-01-03 17:37:38 +0200

    added navigator to calendar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,20 @@
 import CalendarNavigator from '@/components/calendar/calendar-navigator';
 import { Text } from '@/components/ui/text';
+import { formatDate } from '@/lib/date-functions';
+import { useDateStore } from '@/lib/stores/date-store';
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Calendar() {
+  const selectedDay = useDateStore((state) => state.selectedDay);
+
   return (
     <SafeAreaView>
       <CalendarNavigator />
       <ScrollView>
         <View className="flex-1 items-center justify-center gap-8 px-4">
-          {Array.from({ length: 20 }).map((_, index) => (
-            <Text key={index} className="text-lg">
-              Calendar Item {index + 1}
-            </Text>
-          ))}
+          <Text variant="h2">Calendar Tab</Text>
+          <Text variant="muted">{formatDate(selectedDay)}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
