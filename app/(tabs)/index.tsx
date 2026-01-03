2cce4bae7c67dfc022323146daf6b5856925d93f
Pasztor Isti <isticsek@gmail.com>
2026-01-03 15:59:17 +0200

    add navigator to the main page and fill page with dummy text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
+import CalendarNavigator from '@/components/calendar/calendar-navigator';
 import { Text } from '@/components/ui/text';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Calendar() {
   return (
-    <>
-      <View className="flex-1 items-center justify-center gap-8 px-4">
-        <Text variant="h1">Home screen</Text>
-      </View>
-    </>
+    <SafeAreaView>
+      <CalendarNavigator />
+      <ScrollView>
+        <View className="flex-1 items-center justify-center gap-8 px-4">
+          {Array.from({ length: 20 }).map((_, index) => (
+            <Text key={index} className="text-lg">
+              Calendar Item {index + 1}
+            </Text>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
