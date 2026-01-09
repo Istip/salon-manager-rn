0c83a30ede80ea76ce414e9cd4db55d63fc92616
Pasztor Isti <isticsek@gmail.com>
2026-01-09 21:08:09 +0200

    update import

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
-import CalendarEvents from '@/components/calendar/calendar-events';
 import CalendarNavigator from '@/components/calendar/calendar-navigator';
+import Events from '@/components/calendar/events';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Calendar() {
   return (
     <SafeAreaView className="flex-1">
       <CalendarNavigator />
-      <CalendarEvents />
+      <Events />
     </SafeAreaView>
   );
 }
