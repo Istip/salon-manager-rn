1ad3807fb6d9cda6406a6b89a942fd83c7f05729
Pasztor Isti <isticsek@gmail.com>
2026-01-09 22:13:00 +0200

    no appointments component

---
+++ components/utils/no-appointment.tsx
@@ -0,0 +1,22 @@
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
+import { CalendarX2Icon } from 'lucide-react-native';
+import { View } from 'react-native';
+
+const NoAppointment = () => {
+  return (
+    <View className="flex items-center justify-center py-10">
+      <View className="mb-4 animate-pulse rounded-2xl bg-primary p-6">
+        <Icon as={CalendarX2Icon} size={24} className="text-foreground" />
+      </View>
+      <Text variant="h3" className="text-center text-muted-foreground">
+        No appointments
+      </Text>
+      <Text variant="h4" className="text-center font-light text-muted">
+        for today
+      </Text>
+    </View>
+  );
+};
+
+export default NoAppointment;
