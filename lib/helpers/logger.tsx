3dd7de1d0becc8eac0e988f8e32dbf7fecfb4e5b
Pasztor Isti <isticsek@gmail.com>
2026-01-14 16:19:27 +0200

    update logme with new prop

--- lib/helpers/logger.tsx
+++ lib/helpers/logger.tsx
@@ -1,3 +1,7 @@
-export const logme = (data: any) => {
-  console.log(`${data}: `, JSON.stringify(data, null, 2));
+export const logme = (data: any, name?: string) => {
+  if (!name) {
+    console.log(JSON.stringify(data, null, 2));
+  } else {
+    console.log(`${name}: `, JSON.stringify(data, null, 2));
+  }
 };
