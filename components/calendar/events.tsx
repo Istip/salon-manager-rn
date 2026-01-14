7b602fac42e514ee9c88b1a5733893c967475445
Pasztor Isti <isticsek@gmail.com>
2026-01-14 22:09:39 +0200

    add daily income tracker + pimping styles

--- components/calendar/events.tsx
+++ components/calendar/events.tsx
@@ -9,40 +9,74 @@ import { timestamps } from '@/lib/helpers/date-functions';
 import { useCalendarEvents } from '@/lib/hooks/use-calendar-events';
 import { useDateStore } from '@/lib/stores/date-store';
 import { FlashList } from '@shopify/flash-list';
-import { CalendarCheck, CalendarDays, CalendarRange } from 'lucide-react-native';
+import { CalendarCheck, CalendarDays, CalendarRange, CheckCircle2 } from 'lucide-react-native';
 import { useState } from 'react';
 import { View } from 'react-native';
 
 const EventsHeader = ({
   onFilterChange,
   filter,
+  getAllEventsPrice,
+  getDoneEventsPrice,
+  noEvents,
 }: {
   onFilterChange: (filter: 'all' | 'workingHours' | 'appointments') => void;
   filter: 'all' | 'workingHours' | 'appointments';
+  getAllEventsPrice: () => number;
+  getDoneEventsPrice: () => number;
+  noEvents: () => boolean;
 }) => {
+  const allDone = getDoneEventsPrice() === getAllEventsPrice();
+
   return (
-    <View className="mb-4 flex flex-row gap-2">
-      <Button
-        variant={`${filter === 'all' ? 'outline' : 'ghost'}`}
-        className="w-full flex-1"
-        onPress={() => onFilterChange('all')}>
-        <Icon size="18" as={CalendarDays} />
-        <Text variant="small">All</Text>
-      </Button>
-      <Button
-        variant={`${filter === 'workingHours' ? 'outline' : 'ghost'}`}
-        className="w-full flex-1"
-        onPress={() => onFilterChange('workingHours')}>
-        <Icon size="18" as={CalendarRange} />
-        <Text variant="small">Work</Text>
-      </Button>
-      <Button
-        variant={`${filter === 'appointments' ? 'outline' : 'ghost'}`}
-        className="w-full flex-1"
-        onPress={() => onFilterChange('appointments')}>
-        <Icon size="18" as={CalendarCheck} />
-        <Text variant="small">Events</Text>
-      </Button>
+    <View>
+      <View className="mb-4 flex flex-row gap-2">
+        <Button
+          variant={`${filter === 'all' ? 'outline' : 'ghost'}`}
+          className="w-full flex-1"
+          onPress={() => onFilterChange('all')}>
+          <Icon size="18" as={CalendarDays} />
+          <Text variant="small">All</Text>
+        </Button>
+        <Button
+          variant={`${filter === 'workingHours' ? 'outline' : 'ghost'}`}
+          className="w-full flex-1"
+          onPress={() => onFilterChange('workingHours')}>
+          <Icon size="18" as={CalendarRange} />
+          <Text variant="small">Work</Text>
+        </Button>
+        <Button
+          variant={`${filter === 'appointments' ? 'outline' : 'ghost'}`}
+          className="w-full flex-1"
+          onPress={() => onFilterChange('appointments')}>
+          <Icon size="18" as={CalendarCheck} />
+          <Text variant="small">Events</Text>
+        </Button>
+      </View>
+      {!noEvents() && (
+        <View
+          className={`mb-4 flex flex-row items-center justify-center gap-2 rounded-xl border py-2 ${allDone ? 'border-primary bg-primary/50' : 'border-primary/20 bg-primary/5'}`}>
+          {allDone && (
+            <View>
+              <Icon as={CheckCircle2} className="text-primary" />
+            </View>
+          )}
+          <Text variant="small">
+            Daily income:{' '}
+            <Text
+              variant="small"
+              className={`${!allDone ? 'text-muted-foreground' : 'text-primary'}`}>
+              {getDoneEventsPrice()}
+            </Text>{' '}
+            /{' '}
+            <Text
+              variant="small"
+              className={`${!allDone ? 'text-muted-foreground' : 'text-primary'}`}>
+              {getAllEventsPrice()} LEI
+            </Text>
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -72,6 +106,28 @@ const Events = () => {
   const { events, loading } = useCalendarEvents();
   const selectedDay = useDateStore((state) => state.selectedDay);
 
+  const noEvents = () => {
+    return Object.keys(events).length === 0;
+  };
+
+  const getAllEventsPrice = () => {
+    let total = 0;
+    Object.values(events).forEach((event) => {
+      total += event.price || 0;
+    });
+    return total;
+  };
+
+  const getDoneEventsPrice = () => {
+    let total = 0;
+    Object.values(events).forEach((event) => {
+      if (event.done) {
+        total += event.price || 0;
+      }
+    });
+    return total;
+  };
+
   const displayData = getFilteredAppointments(filteredAppointments, timestamps, events);
 
   const renderEventOrEmpty = (item: string) => {
@@ -96,7 +152,13 @@ const Events = () => {
       keyExtractor={(item) => `${selectedDay.toDateString()}-${item}`}
       showsVerticalScrollIndicator={false}
       ListHeaderComponent={
-        <EventsHeader filter={filteredAppointments} onFilterChange={setFilteredAppointments} />
+        <EventsHeader
+          noEvents={noEvents}
+          filter={filteredAppointments}
+          onFilterChange={setFilteredAppointments}
+          getAllEventsPrice={getAllEventsPrice}
+          getDoneEventsPrice={getDoneEventsPrice}
+        />
       }
       ListEmptyComponent={filteredAppointments === 'appointments' ? <NoAppointment /> : null}
     />
